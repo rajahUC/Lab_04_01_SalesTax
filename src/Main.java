1e6160732f26f41e2099f511a638c96989c21ab0
rajahUC <rajaho@mail.uc.edu>
2023-06-05 23:16:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 20.50;
+        final double SALES_TAX = 0.05;
+        double totalPrice = 0;
+    }
+}
